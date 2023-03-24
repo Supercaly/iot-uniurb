d7e7f44ade1666e9a008390ad70cabf48bef648d
Lorenzo Calisti <djonline96@gmail.com>
2023-03-24 10:08:09 +0100

    Add `version.h` to manage the firmware version

--- src/common.h
+++ src/common.h
@@ -1,7 +1,13 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+/*
+ * Common header included throughout the project 
+ * to easily manage all the includes.
+ */
+
 #include "config.h"
+#include "version.h"
 
 #include <Arduino.h>
 
--- src/config.h
+++ src/config.h
@@ -1,11 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/*
+ * Global configuration header.
+ * 
+ * Here we put all the configurations for the
+ * various elements of the project.
+ */
+
 // Enable Debug Mode.
 #define IS_DEBUG
 // Print the value of sensors after reading them.
 #define PRINT_SENSORS_ON_READ
-
 // Default serial used for print.
 #define LOGGER_SERIAL Serial
 // Serial Baud Rate.
