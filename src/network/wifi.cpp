16911b87b7a8b122987ba463c3761d3624ea42e7
Lorenzo Calisti <djonline96@gmail.com>
2023-04-13 10:27:16 +0200

    MAC: Make `wifi_set_mac_address` method private
    Settings the MAC address requires the WiFi connection do be disabled, but the Wi
    Fi interface to be initialized. With the current wifi implementation we call `WiFi.mode(WIFI_STA) only inside `wifi_connect` so the WiFi interface is never initialized before we try to connect to a network. For this reason having a public method to set the MAC address will result in constant errors.
    
    In the future we could implement the ability to switch between WiFi station and access point, for this we surely need to change the implementation of `wifi.h`; at this time the method could becam public again.

--- src/network/wifi.cpp
+++ src/network/wifi.cpp
@@ -8,6 +8,40 @@
 static int old_backup_state = HIGH;
 #endif  // HAS_BACKUP_WIFI
 
+// TODO: Make function set_mac_address publicly available.
+//  Currently we call WiFi.mode(WIFI_STA) inside wifi_connect, so the WiFi
+//  interface is not initialized until we try to connect to a network.
+//  Using a public method to set the MAC address before connecting will always
+//  result in errors since the WiFi interface is never initialize.
+//  At the moment it's good setting the MAC privately only when connecting to a
+//  network, but, in case we implement the access piont we can let the user
+//  change the MAC.
+static void set_mac_address(String mac) {
+  int mac_val[6];
+  uint8_t mac_bytes[6];
+
+  // Parse MAC address string to bytes
+  int sz = sscanf(mac.c_str(), "%x:%x:%x:%x:%x:%x%*c",
+                  &mac_val[0], &mac_val[1], &mac_val[2],
+                  &mac_val[3], &mac_val[4], &mac_val[5]);
+  if (sz != 6) {
+    Log.errorln("set_mac_address: invalid MAC address: '" + mac + "'");
+    return;
+  }
+
+  for (int i = 0; i < 6; ++i) {
+    mac_bytes[i] = (uint8_t)mac_val[i];
+  }
+  esp_err_t err = esp_wifi_set_mac(WIFI_IF_STA, mac_bytes);
+  if (err == ESP_OK) {
+    Log.debugln("set_mac_address: new MAC address: "
+                + wifi_get_mac_address());
+  } else {
+    Log.traceln("set_mac_address: error setting MAC: "
+                + String(esp_err_to_name(err)));
+  }
+}
+
 bool wifi_connect(const char *ssid, const char *pwd,
                   int max_retry, int pause) {
   Log.traceln("wifi_connect: connecting to SSID: '" + String(ssid)
@@ -58,11 +92,6 @@ String wifi_get_ip() {
 
 String wifi_get_mac_address() {
   return WiFi.macAddress();
-}
-
-bool wifi_set_mac_address(uint8_t *mac) {
-  esp_err_t err = esp_wifi_set_mac(WIFI_IF_STA, mac);
-  return err == ESP_OK;
 }
 
 #ifdef HAS_BACKUP_WIFI
