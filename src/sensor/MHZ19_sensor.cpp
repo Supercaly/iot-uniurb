a7d6f29dc2bdcff9d77b5914de161dad2493fb6f
Lorenzo Calisti <djonline96@gmail.com>
2024-01-11 15:54:44 +0100

    MHZ19B: Filter out all unexpected peaks
    This commit adds acheck to filter out all unexpected peaks returned by the sensor.

--- src/sensor/MHZ19_sensor.cpp
+++ src/sensor/MHZ19_sensor.cpp
@@ -11,6 +11,7 @@ MHZ19_Sensor MHZ19Sensor;
 bool MHZ19_Sensor::on_init() {
   MHZ19_SERIAL.begin(MHZ19_BAUD_RATE, SERIAL_8N1, MHZ19_RX, MHZ19_TX);
   _mhz.begin(&MHZ19_SERIAL);
+  _mhz.setPPM(5000);
 
   delay(MHZ19_INIT_DELAY_MS);
   app_traceln("MHZ19_Sensor::init: sensor initialized");
@@ -21,22 +22,31 @@ bool MHZ19_Sensor::on_measure() {
   int currentCo2 = 0, minCo2 = 0, maxCo2 = 0;
 
   app_traceln("MHZ19_Sensor::measure: reading sensor values");
-  for (int i = 0; i < SENSOR_AVG_WINDOW; ++i) {
-    _mhz.measure();
+  int i = 0, k = 0;
+  while (i < SENSOR_AVG_WINDOW && k < 100) {
+    // for (int i = 0; i < SENSOR_AVG_WINDOW; ++i) {
+    int err    = _mhz.measure();
     currentCo2 = _mhz.getCO2();
 
-    if (currentCo2 < minCo2) {
-      minCo2 = currentCo2;
+    if (err == MHZCO2_OK && currentCo2 <= 5500) {
+      if (currentCo2 < minCo2) {
+        minCo2 = currentCo2;
+      }
+      if (currentCo2 > maxCo2) {
+        maxCo2 = currentCo2;
+      }
+      _co2 += currentCo2;
+      i++;
     }
-    if (currentCo2 > maxCo2) {
-      maxCo2 = currentCo2;
-    }
-    _co2 += currentCo2;
+    k++;
+
     delay(SENSOR_AVG_DELAY_MS);
   }
-  _co2 -= minCo2;
-  _co2 -= maxCo2;
-  _co2 /= (SENSOR_AVG_WINDOW - 2);
+  if (i > 2) {
+    _co2 -= minCo2;
+    _co2 -= maxCo2;
+    _co2 /= (i - 2);
+  }
 
 #ifdef PRINT_SENSORS_ON_READ
   app_infoln("MHZ19 Co2: " + String(_co2));
